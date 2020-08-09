2d68acf47423502ad3bfd22d7e6a4cd48a00e8ea
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2020-08-08 22:51:28 -0400

    Goonade does a 5x5x5 area

--- src/main/java/com/direwolf20/diregoo/common/entities/GoonadeEntity.java
+++ src/main/java/com/direwolf20/diregoo/common/entities/GoonadeEntity.java
@@ -3,7 +3,6 @@ package com.direwolf20.diregoo.common.entities;
 import com.direwolf20.diregoo.DireGoo;
 import com.direwolf20.diregoo.common.blocks.GooBase;
 import com.direwolf20.diregoo.common.items.ModItems;
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
@@ -12,6 +11,9 @@ import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.registries.ObjectHolder;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GoonadeEntity extends ProjectileBase {
     @ObjectHolder(DireGoo.MOD_ID + ":goonadeentity")
@@ -29,12 +31,14 @@ public class GoonadeEntity extends ProjectileBase {
     @Override
     protected void onImpact(RayTraceResult result) {
         if (!world.isRemote) {
-            BlockPos pos = new BlockPos(result.getHitVec().getX(), result.getHitVec().getY(), result.getHitVec().getZ());
-            BlockState inBlockState = world.getBlockState(pos);
-            if (inBlockState.getBlock() instanceof GooBase)
+            BlockPos hitPos = new BlockPos(result.getHitVec().getX(), result.getHitVec().getY(), result.getHitVec().getZ());
+            List<BlockPos> area = BlockPos.getAllInBox(hitPos.add(-2, -2, -2), hitPos.add(2, 2, 2))
+                    .filter(blockPos -> world.getBlockState(blockPos).getBlock() instanceof GooBase)
+                    .map(BlockPos::toImmutable)
+                    .collect(Collectors.toList());
+            for (BlockPos pos : area)
                 GooBase.resetBlock((ServerWorld) world, pos);
             remove();
-
         }
     }
 
