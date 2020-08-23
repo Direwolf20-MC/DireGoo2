8ee4124ee558dceecd6926e8e557babe135e43e5
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2020-08-23 00:40:05 -0400

    Add BlockStates to Poison goo - need to refine it (and maybe add a few more) but its getting there.

--- src/main/java/com/direwolf20/diregoo/common/blocks/GooBlockPoison.java
+++ src/main/java/com/direwolf20/diregoo/common/blocks/GooBlockPoison.java
@@ -1,6 +1,9 @@
 package com.direwolf20.diregoo.common.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.state.IntegerProperty;
+import net.minecraft.state.StateContainer;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.server.ServerWorld;
@@ -9,32 +12,81 @@ import java.util.Random;
 
 public class GooBlockPoison extends GooBase {
 
+    public static final IntegerProperty GENERATION = IntegerProperty.create("generation", 0, 5);
+
 
     @Override
     public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
         if (!worldIn.isAreaLoaded(pos, 3))
             return; // Forge: prevent loading unloaded chunks when checking neighbor's light and spreading
-        if (rand.nextDouble() < 0.5) //The percent chance it decays rather than spreads. Lower == more spread.
-            this.resetBlock(worldIn, pos, false, 80);
-        else {
-            BlockPos gooPos = spreadGoo(state, worldIn, pos, rand);
-            worldIn.getPendingBlockTicks().scheduleTick(gooPos, this, 5);
-            worldIn.getPendingBlockTicks().scheduleTick(pos, this, 5);
+        if (rand.nextDouble() <= decayChance(state)) { //The percent chance it decays rather than spreads. Lower == more spread.
+            this.resetBlock(worldIn, pos, true, 80);
+        } else {
+            System.out.println("Generation " + state.get(GENERATION) + " is spreading");
+            spreadGoo(state, worldIn, pos, rand);
         }
+    }
+
+    private double decayChance(BlockState gooState) {
+        int generation = gooState.get(GENERATION);
+        switch (generation) {
+            case 0:
+                return 0.05;
+            case 1:
+                return 0.10;
+            case 2:
+                return 0.10;
+            case 3:
+                return 0.10;
+            case 4:
+                return 0.15;
+            case 5:
+                return 0.50;
+        }
+        return 1.0;
     }
 
     @Override
     public BlockPos spreadGoo(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
-        int x = rand.nextInt(Direction.values().length);
-        Direction direction = Direction.values()[x];
+        if (state.get(GENERATION) % 2 == 0) {
+            for (Direction direction : Direction.values()) {
+                BlockPos checkPos = pos.offset(direction);
+                BlockState oldState = worldIn.getBlockState(checkPos);
 
-        BlockPos checkPos = pos.offset(direction);
-        BlockState oldState = worldIn.getBlockState(checkPos);
+                if (oldState.getBlock() instanceof GooBlock && !(oldState.getBlock() instanceof GooBlockPoison)) {
+                    int generation = state.get(GENERATION);
+                    if (generation < 5)
+                        generation++;
+                    worldIn.setBlockState(checkPos, ModBlocks.GOO_BLOCK_POISON.get().getDefaultState().with(GENERATION, generation));
+                    worldIn.setBlockState(pos, ModBlocks.GOO_BLOCK_POISON.get().getDefaultState().with(GENERATION, generation));
+                    worldIn.getPendingBlockTicks().scheduleTick(checkPos, this, 5);
+                }
+                worldIn.getPendingBlockTicks().scheduleTick(pos, this, 5);
+            }
+            return BlockPos.ZERO;
+        } else {
+            int x = rand.nextInt(Direction.values().length);
+            Direction direction = Direction.values()[x];
 
-        if (oldState.getBlock() instanceof GooBlock && !(oldState.getBlock() instanceof GooBlockPoison)) {
-            worldIn.setBlockState(checkPos, ModBlocks.GOO_BLOCK_POISON.get().getDefaultState());
-            return checkPos;
+            BlockPos checkPos = pos.offset(direction);
+            BlockState oldState = worldIn.getBlockState(checkPos);
+
+            if (oldState.getBlock() instanceof GooBlock && !(oldState.getBlock() instanceof GooBlockPoison)) {
+                int generation = state.get(GENERATION);
+                if (generation < 5)
+                    generation++;
+                worldIn.setBlockState(checkPos, ModBlocks.GOO_BLOCK_POISON.get().getDefaultState().with(GENERATION, generation));
+                worldIn.setBlockState(pos, ModBlocks.GOO_BLOCK_POISON.get().getDefaultState().with(GENERATION, generation));
+                worldIn.getPendingBlockTicks().scheduleTick(checkPos, this, 5);
+            }
+            worldIn.getPendingBlockTicks().scheduleTick(pos, this, 5);
+            return BlockPos.ZERO;
         }
-        return BlockPos.ZERO;
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        super.fillStateContainer(builder);
+        builder.add(GENERATION);
     }
 }
