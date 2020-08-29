c88dc56d0b8152045f01ae82fd7d3918eca8592f
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2020-08-28 20:51:42 -0400

    Cleargoo Command now kills goo spread entities. Otherwise they'd survive and make more goo!

--- src/main/java/com/direwolf20/diregoo/common/commands/CommandClearGoo.java
+++ src/main/java/com/direwolf20/diregoo/common/commands/CommandClearGoo.java
@@ -1,6 +1,7 @@
 package com.direwolf20.diregoo.common.commands;
 
 import com.direwolf20.diregoo.common.blocks.GooBase;
+import com.direwolf20.diregoo.common.entities.GooSpreadEntity;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
@@ -10,6 +11,7 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.server.ServerWorld;
@@ -33,12 +35,17 @@ public class CommandClearGoo implements Command<CommandSource> {
         int range = IntegerArgumentType.getInteger(context, "range");
         PlayerEntity player = context.getSource().asPlayer();
         BlockPos hitPos = new BlockPos(player.getPosX(), player.getPosY(), player.getPosZ());
-        List<BlockPos> area = BlockPos.getAllInBox(hitPos.add(-range, -range, -range), hitPos.add(range, range, range))
+        BlockPos minPos = hitPos.add(-range, -range, -range);
+        BlockPos maxPos = hitPos.add(range, range, range);
+        List<BlockPos> area = BlockPos.getAllInBox(minPos, maxPos)
                 .filter(blockPos -> player.getEntityWorld().getBlockState(blockPos).getBlock() instanceof GooBase)
                 .map(BlockPos::toImmutable)
                 .collect(Collectors.toList());
         for (BlockPos pos : area)
             GooBase.resetBlock((ServerWorld) player.getEntityWorld(), pos, false, 80);
+        List<GooSpreadEntity> gooSpreadEntities = player.getEntityWorld().getEntitiesWithinAABB(GooSpreadEntity.class, new AxisAlignedBB(minPos, maxPos));
+        for (GooSpreadEntity gooSpreadEntity : gooSpreadEntities)
+            gooSpreadEntity.remove();
         context.getSource().sendFeedback(new TranslationTextComponent("message.diregoo.command.cleargoo " + area.size()), false);
         return 0;
     }
